cc447b86ade9dbce7b18b53e7d0548638b6126c0
lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2025-07-12 16:09:59 +0530

    change project from rewear to stackit

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import HomeView from '../views/Home.vue'
-import ShopView from '../views/Shop.vue'
+import LandingView from '../views/Landing.vue'
 import CreateView from '../views/Create.vue'
 
 const routes = [
     { name: 'Home', path: '/', component: HomeView },
-    { name: 'Shop', path: '/shop', component: ShopView },
+    { name: 'Shop', path: '/landing', component: LandingView },
     { name: 'Create', path: '/create', component: CreateView },
 ]
 
