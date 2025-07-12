60e79bde87db0db221645329537f4fc1f04e3570
lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2025-07-12 16:35:38 +0530

    add new route for viewing SFC

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,10 +3,12 @@ import { createWebHistory, createRouter } from 'vue-router'
 import HomeView from '../views/Home.vue'
 import LandingView from '../views/Landing.vue'
 import CreateView from '../views/Create.vue'
+import QuestionView from '../views/QuestionView.vue'
 
 const routes = [
     { name: 'Home', path: '/', component: HomeView },
     { name: 'Shop', path: '/landing', component: LandingView },
+    { name: 'Question', path: '/question:id', component: QuestionView },
     { name: 'Create', path: '/create', component: CreateView },
 ]
 
