56e8f1917e0ff0a194fb191d33cc2f018b88dd3e
lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2025-07-12 15:54:10 +0530

    add main screen

--- rewear/src/App.vue
+++ rewear/src/App.vue
@@ -1,26 +1,5 @@
-<script setup>
-</script>
+<template></template>
 
-<template>
-  <div class="container">
-    <Navbar />
-    <RouterView />
-  </div>
-</template>
+<script setup></script>
 
 <style scoped></style>
-
-<style>
-html,
-body {
-  height: 100%;
-  width: 100%;
-  background-color: #FBFAFE;
-}
-
-html {
-  scroll-padding-top: 5rem;
-  scroll-behavior: smooth;
-  overflow-y: scroll;
-}
-</style>
